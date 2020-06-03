a5373d05acf4e276c93b2f54d687a698cf382fbc
Carsten Otto <git@c-otto.de>
2020-06-03 22:12:11 +0100

    fix PMD issues

--- module-one/src/main/java/de/cotto/playground/moduleone/Hello.java
+++ module-one/src/main/java/de/cotto/playground/moduleone/Hello.java
@@ -1,6 +1,10 @@
 package de.cotto.playground.moduleone;
 
 public class Hello {
+    public Hello() {
+        // default constructor
+    }
+
     public String world() {
         return "Hello World";
     }
--- module-two/src/main/java/de/cotto/playground/moduletwo/HelloWorld.java
+++ module-two/src/main/java/de/cotto/playground/moduletwo/HelloWorld.java
@@ -3,6 +3,10 @@ package de.cotto.playground.moduletwo;
 import de.cotto.playground.moduleone.Hello;
 
 public class HelloWorld {
+    HelloWorld() {
+        // default constructor
+    }
+
     public String helloWorld() {
         return new Hello().world() + "!";
     }
