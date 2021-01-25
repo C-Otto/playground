b7faa54b7acb733d00c3b59b1e48c4691a308f8b
Carsten Otto <git@c-otto.de>
2021-01-25 13:49:58 +0000

    Example: avoid instantiating new objects inside loops

--- subproject-one/src/main/java/de/cotto/playground/subprojectone/Hello.java
+++ subproject-one/src/main/java/de/cotto/playground/subprojectone/Hello.java
@@ -1,5 +1,8 @@
 package de.cotto.playground.subprojectone;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Hello {
     public Hello() {
         // default constructor
@@ -7,5 +10,16 @@ public class Hello {
 
     public String world() {
         return "Hello World";
+    }
+
+    public Collection<Car> checkCollection() {
+        Collection<Car> cars = new ArrayList<>();
+        for(int i = 0; i < 3; ++i) {
+            cars.add(new Car());
+        }
+        return cars;
+    }
+
+    private static class Car {
     }
 }
--- subproject-two/src/main/java/module-info.java
+++
@@ -1,3 +0,0 @@
-module de.cotto.playground.subprojecttwo {
-    requires de.cotto.playground.subprojectone;
-}
